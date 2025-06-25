5b91c7c2d74a93083ff6b32fb3f96f02f7bc90a4
sruthi <sruthirokalla369@gmail.com>
2025-06-25 23:14:57 +0530

    my secondcommit from the intellij using version control

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+        System.out.println("Hello, guys");
+
     }
 }
